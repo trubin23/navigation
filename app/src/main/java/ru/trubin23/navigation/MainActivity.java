0914f4ad284bcbfa2a0b5eb99da5e42bbe0b6a07
trubin23 <trubin23@gmail.com>
2019-03-21 08:29:48 +0500

    add NavHostFragment

--- app/src/main/java/ru/trubin23/navigation/MainActivity.java
+++ app/src/main/java/ru/trubin23/navigation/MainActivity.java
@@ -1,13 +1,20 @@
 package ru.trubin23.navigation;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    NavController mNavController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mNavController = Navigation.findNavController(this, R.id.nav_host_fragment);
     }
 }
